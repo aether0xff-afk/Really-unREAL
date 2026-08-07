19469c3fdf6b4613a1e7dc3947cedcdb35398219
aether0xff-afk <aether0xff@gmail.com>
2026-08-07 23:55:56 +0900

    Expose Instagram ingestion

--- backend/ingest/__init__.py
+++ backend/ingest/__init__.py
@@ -1,3 +1,18 @@
+from .instagram import (
+    InstagramActivitySummary,
+    InstagramExport,
+    InstagramThread,
+    load_instagram_export,
+    repair_meta_text,
+)
 from .kakao import parse_kakao_file, parse_kakao_text
 
-__all__ = ["parse_kakao_file", "parse_kakao_text"]
+__all__ = [
+    "InstagramActivitySummary",
+    "InstagramExport",
+    "InstagramThread",
+    "load_instagram_export",
+    "parse_kakao_file",
+    "parse_kakao_text",
+    "repair_meta_text",
+]
